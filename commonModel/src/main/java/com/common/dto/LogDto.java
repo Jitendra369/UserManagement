ea3f7a095d5ee20de721a21cbaf5877e45bf5cbc
jitendra <onekb.twotb@gmail.com>
2026-02-01 17:05:18 +0530

    adding cahnges for redis

---
+++ commonModel/src/main/java/com/common/dto/LogDto.java
@@ -0,0 +1,53 @@
+package com.common.dto;
+
+import java.time.LocalDateTime;
+
+public class LogDto {
+    private String id;
+    private String projectName;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+    public LogDto() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+}
